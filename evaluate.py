721b586d2be1ece498639d276ef8086169d234ac
Bram Renting <bramrenting@gmail.com>
2024-05-27 22:42:29 +0200

    added evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,68 +1,21 @@
 import random
-import time
-from collections import Counter, defaultdict
-from dataclasses import asdict, dataclass
-from datetime import datetime
-from pathlib import Path
-from typing import Literal
-import plotly.express as px
+from collections import defaultdict
+from dataclasses import asdict
 
 import numpy as np
+import plotly.express as px
 import torch
 import tyro
-from gymnasium.spaces import flatten_space
-from tarware import ObserationType, Warehouse, RewardType
+from tarware import Warehouse
 
-from agent import MultiAgentModule, HeuristicPolicyModule, PPOPolicyModule
-
-
-@dataclass
-class WarehouseConfig:
-    column_height: int = 8
-    shelf_rows: int = 2
-    shelf_columns: int = 5
-    n_agvs: int = 8
-    n_pickers: int = 4
-    msg_bits: int = 0
-    sensor_range: int = 1
-    request_queue_size: int = 20
-    max_inactivity_steps: int | None = None
-    max_steps: int = 500
-    reward_type: RewardType = RewardType.INDIVIDUAL
-    observation_type: ObserationType = ObserationType.FLATTENED
-    normalised_coordinates: bool = False
-    render_mode: str | None = None
-    action_masking: bool = True
-    sample_collection: Literal["all", "masking", "relevant"] = "masking"
-    no_observations: bool = False
-    improved_masking: bool = True
-
-
-@dataclass
-class Config:
-    warehouse: WarehouseConfig
-
-    exp_name: str = Path(__file__).stem
-    """the name of this experiment"""
-    seed: int = 1
-    """seed of the experiment"""
-    torch_deterministic: bool = True
-    """if toggled, `torch.backends.cudnn.deterministic=False`"""
-    cuda: bool = False
-    """if toggled, cuda will be enabled by default"""
-    capture_video: bool = False
-    """whether to capture videos of the agent performances (check out `videos` folder)"""
-
-    # Multi-agent settings
-    model: str = "models/ippo_24-05-15_14:24:50"
-
-    num_iterations: int = 10
-
-    learning_rate: float = 3e-4
+from agent import MultiAgentModule
+from train import Config
 
 
 def main():
     config = tyro.cli(Config)
+    config.num_iterations = 1
+    config.model = "models/ippo_24-05-24_19:42:38"
 
     # TRY NOT TO MODIFY: seeding
     random.seed(config.seed)
@@ -77,28 +30,31 @@ def main():
     env_config = config.warehouse
     env = Warehouse(**asdict(env_config))
 
-    model_type = config.model.split("/")[-1].split("_")[0]
-    if model_type == "snppo":
+    if config.ma_algorithm == "snppo":
         agent_policy_mapping = {agent_id: agent_id.split("_")[0] for agent_id in env.possible_agents}
-    elif model_type == "ippo":
+    elif config.ma_algorithm == "ippo":
         agent_policy_mapping = {agent_id: agent_id for agent_id in env.possible_agents}
     
-    agents = MultiAgentModule(agent_policy_mapping, env, config, module_class=PPOPolicyModule)
-    # agents.load(config.model)
+    agents = MultiAgentModule(agent_policy_mapping, env, config)
+    agents.load(config.model)
 
     next_obs, infos = env.reset(seed=config.seed)
 
     actions_layout_data = {agent_id: np.zeros((env.grid_size[0] + 1, env.grid_size[1]), dtype=int) for agent_id in next_obs.keys()}
     actions_layout_relevant_data = {agent_id: np.zeros((env.grid_size[0] + 1, env.grid_size[1]), dtype=int) for agent_id in next_obs.keys()}
 
+    log_metrics = defaultdict(list)
     for iteration in range(config.num_iterations):
         print(iteration)
         # agents.clear_buffers()
-        next_obs, infos = env.reset(seed=config.seed)
+        # env = Warehouse(**asdict(env_config))
+        # agents = MultiAgentModule(agent_policy_mapping, env, config)
+        next_obs, infos = env.reset(seed=config.seed + iteration)
         next_done = {agent_id: False for agent_id in next_obs.keys()}
         episode_done = False
-        log_metrics = defaultdict(list)
         while not episode_done:
+
+            env.render()
 
             # agents.add_observations_dones(next_obs, next_done)
             # ALGO LOGIC: action logic
@@ -106,7 +62,8 @@ def main():
                 actions = agents.get_actions(next_obs)
             
             for agent_id, action in actions.items():
-                location = env.item_loc_dict.get(int(action), (-1, 0))
+                action_shifted = action + len(env.goals) if agent_id.startswith("PICKER") and action != 0 else action
+                location = env.item_loc_dict.get(int(action_shifted), (-1, 0))
                 actions_layout_data[agent_id][location] += 1
                 if not infos[agent_id]["busy"]:
                     actions_layout_relevant_data[agent_id][location] += 1
@@ -128,9 +85,10 @@ def main():
                 for metric_name, data in infos["__common__"].items():
                     log_metrics[f"info/{metric_name}"].append(float(data))
 
+        print(f"pickrate: {sum(log_metrics['info/pickrate']) / len(log_metrics['info/pickrate'])}")
 
-        log_data = {name: sum(data) / len(data) if len(data) > 0 else 0 for name, data in log_metrics.items()}
-        # agents.train()
+    log_data = {name: sum(data) / len(data) if len(data) > 0 else 0 for name, data in log_metrics.items()}
+    # agents.train()
 
     for name, data in log_data.items():
         print(f"{name}: {data}")
